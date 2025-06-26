7c89ff72d54a0025b95e04068789d2b3c5b40ff7
nemo <nemo1002forwork@gmail.com>
2025-06-26 17:38:01 +0800

    attach verification files

--- src/verification/StakeCore.sol
+++ src/verification/StakeCore.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {IERC20} from "./IERC20.sol";
+import {SafeERC20} from "./SafeERC20.sol";
+import {ReentrancyGuard} from "./ReentrancyGuard.sol";
+import {Math} from "./Math.sol";
+
 
 // import "forge-std/console.sol";
 
